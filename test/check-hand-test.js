d29ec6151667410b3c46aa70b4ad78f3c22c8c90
Shikhar Subedi <shikharsubedi@Shikhars-MacBook-Pro.local>
2019-06-18 21:48:25 -0700

    updated travis ci

--- test/check-hand-test.js
+++ test/check-hand-test.js
@@ -1,11 +1,12 @@
-const { checkHand } = require('../check-hand')
+const { checkHand } = require('../src/check-hand')
 const { expect } = require('chai')
 
-
 describe('test winning hands in poker', function () {
-    it('test straigt flush', function () {
-        const hand = ['11-S','10-S','9-S', '8-S', '7-S']
-        const resultString = checkHand(hand)
-        expect(resultString).to.equal('straight flush')
-    })
+  it('test straigt flush', function () {
+    const hand = ['11-S', '10-S', '9-S', '8-S', '7-S']
+    const resultString = checkHand(hand)
+    expect(resultString).to.equal('straight flush')
+    const hand2 = ['12-S', '1-S', '13-S', '10-S', '11-S']
+    expect(checkHand(hand2)).to.equal('straight flush')
+  })
 })
